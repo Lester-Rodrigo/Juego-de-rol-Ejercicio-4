e4be2500a5fbca18cf41cbc924f9b0724f68dbef
Lester-Rodrigo <alrodrinew@gmail.com>
2025-10-05 13:56:55 -0600

    Modificación en el método de activación de alguna habilidades

--- Acido.java
+++ Acido.java
@@ -1,7 +1,29 @@
+import java.util.Random;
+
 public class Acido extends Habilidades{
 
     public Acido() {
         super("Acido", "Quimico");
     }
     
+    @Override
+    public boolean activarHabilidad () {
+            boolean activacion;
+            Random numero = new Random();
+            int x = numero.nextInt(100)+1;
+            if (0<= x && x<100){
+                activacion = true;
+                return activacion;
+            }
+            else{
+                activacion = false;
+                return activacion;
+            }
+        }
+
+    public void Derretir (Heroe heroe) {
+        if (this.activarHabilidad() == true) {
+            heroe.setDefensa(heroe.getDefensa()-5);
+        }
+    }
 }
--- Sangrado.java
+++ Sangrado.java
@@ -1,7 +1,33 @@
+import java.util.Random;
+
 public class Sangrado extends Habilidades{
 
     public Sangrado() {
         super("Sangrado", "Herida");
     }
     
+    @Override
+    public boolean activarHabilidad () {
+            boolean activacion;
+            Random numero = new Random();
+            int x = numero.nextInt(50)+1;
+            if (0<= x && x<51){
+                activacion = true;
+                return activacion;
+            }
+            else{
+                activacion = false;
+                return activacion;
+            }
+        }
+
+    public void Regenerar (Heroe heroe, Monstruos monstruo) {
+        if (this.activarHabilidad() == true) {
+            if (monstruo.getAtaque() > heroe.getDefensa()) {
+                double sangrePerdida = (monstruo.getAtaque()*30)/100; 
+                heroe.setVida(heroe.getVida() - sangrePerdida );
+                heroe.setAtaque(heroe.getAtaque()-5);
+            }
+        }
+    }
 }
